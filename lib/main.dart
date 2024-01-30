6abdb9dc6759d213cf4c868becaadf6a6a4293b9
Jhoiti <jhoitimamoru@email.com>
2024-01-30 20:55:20 -0300

    alteracao ignore

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bitcoinapp/Home.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(MaterialApp(
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    ));
-    //teste
